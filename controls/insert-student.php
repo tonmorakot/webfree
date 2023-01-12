f0b156ef9b6b60c014f1c5e6dcc7b1772075f910
Morakot Thongprom <tonmorakot@gmail.com>
2023-01-12 12:01:52 +0700

    dev

---
+++ controls/insert-student.php
@@ -0,0 +1,58 @@
+<?php
+
+  $s_id = $_POST["s_id"];
+  $s_name = $_POST["s_name"];
+  $s_lastname = $_POST["s_lastname"];
+  $s_email = $_POST["s_email"];
+  $s_tel = $_POST["s_tel"];
+  $s_line = $_POST["s_line"];
+  $s_address = $_POST["s_address"];
+  $s_moo = $_POST["s_moo"];
+  $s_road = $_POST["s_road"];
+  $s_province = $_POST["s_province"];
+  $s_amphoe = $_POST["s_amphoe"];
+  $s_tumbon = $_POST["s_tumbon"];
+  $s_post = $_POST["s_post"];
+
+  $s_day = $_POST["s_day"];
+  $s_month = $_POST["s_month"];
+  $s_year = $_POST["s_year"];
+
+  $age = 2566 - $s_year;
+
+  echo "คุณ " .$s_name. " มีอายุ = " .$age. " ปี";
+  echo "<br>";
+  echo $s_id;
+  echo "<br>";
+  echo $s_name;
+  echo "<br>";
+  echo $s_lastname;
+  echo "<br>";
+  echo $s_email;
+  echo "<br>";
+  echo $s_tel;
+  echo "<br>";
+  echo $s_line;
+  echo "<br>";
+  echo $s_address;
+  echo "<br>";
+  echo $s_moo;
+  echo "<br>";
+  echo $s_road;
+  echo "<br>";
+  echo $s_province;
+  echo "<br>";
+  echo $s_amphoe;
+  echo "<br>";
+  echo $s_tumbon;
+  echo "<br>";
+  echo $s_post;
+  echo "<br>";
+  echo $s_day;
+  echo "<br>";
+  echo $s_month;
+  echo "<br>";
+  echo $s_year;
+  echo "<br>";
+
+?>
--- register.php
+++ register.php
@@ -8,7 +8,183 @@
     <?php include("./components/nav.php"); ?>
 
     <div class="container-fluid mt-3">
-      <h1>สมัครสมาชิก</h1>  
+      <h1>สมัครสมาชิก</h1>
+
+      <form method="post" action="controls/insert-student.php">
+
+        <div class="row">
+          <div class="col-md-4">
+
+            <label class="form-label">รหัสนักศึกษา <span style="color: red;">*</span></label>
+            <input type="text" class="form-control" placeholder="กรุณากรอกรหัสนักศึกษา" name="s_id">
+
+          </div>
+          <div class="col-md-4">
+
+            <label class="form-label">ชื่อ <span style="color: red;">*</span></label>
+            <input type="text" class="form-control" placeholder="กรุณากรอกชื่อ" name="s_name">
+          
+          </div>
+          <div class="col-md-4">
+
+            <label class="form-label">นามสกุล <span style="color: red;">*</span></label>
+            <input type="text" class="form-control" placeholder="กรุณากรอกนามสกุล" name="s_lastname">
+          
+          </div>
+        </div>
+
+        <div class="row mt-3">
+          <div class="col-md-4">
+
+            <label class="form-label">อีเมล <span style="color: red;">*</span></label>
+            <input type="text" class="form-control" placeholder="กรุณากรอกอีเมล" name="s_email">
+
+          </div>
+          <div class="col-md-4">
+
+            <label class="form-label">เบอร์โทร <span style="color: red;">*</span></label>
+            <input type="text" class="form-control" placeholder="กรุณากรอกเบอร์โทร" name="s_tel">
+          
+          </div>
+          <div class="col-md-4">
+
+            <label class="form-label">Line ID <span style="color: red;">*</span></label>
+            <input type="text" class="form-control" placeholder="กรุณากรอก Line ID" name="s_line">
+          
+          </div>
+        </div>
+
+        <div class="row mt-3">
+          <div class="col-md-3">
+
+            <label class="form-label">บ้านเลขที่ <span style="color: red;">*</span></label>
+            <input type="text" class="form-control" placeholder="กรุณากรอกบ้านเลขที่" name="s_address">
+
+          </div>
+          <div class="col-md-3">
+
+            <label class="form-label">หมู่ที่ <span style="color: red;">*</span></label>
+            <input type="text" class="form-control" placeholder="กรุณากรอกหมู่ที่" name="s_moo">
+          
+          </div>
+          <div class="col-md-3">
+
+            <label class="form-label">ถนน <span style="color: red;">*</span></label>
+            <input type="text" class="form-control" placeholder="กรุณากรอกถนน" name="s_road">
+          
+          </div>
+          <div class="col-md-3">
+
+            <label class="form-label">จังหวัด <span style="color: red;">*</span></label>
+            <input type="text" class="form-control"  placeholder="กรุณากรอกจังหวัด" name="s_province">
+          
+          </div>
+        </div>
+
+        <div class="row mt-3">
+          <div class="col-md-4">
+
+            <label class="form-label">อำเภอ <span style="color: red;">*</span></label>
+            <input type="text" class="form-control" placeholder="กรุณากรอกอำเภอ" name="s_amphoe">
+
+          </div>
+          <div class="col-md-4">
+
+            <label class="form-label">ตำบล <span style="color: red;">*</span></label>
+            <input type="text" class="form-control" placeholder="กรุณากรอกตำบล" name="s_tumbon">
+          
+          </div>
+          <div class="col-md-4">
+
+            <label class="form-label">รหัสไปรษณีย์ <span style="color: red;">*</span></label>
+            <input type="text" class="form-control" placeholder="กรุณากรอกรหัสไปรษณีย์" name="s_post">
+          
+          </div>
+        </div>
+
+        <div class="row mt-3">
+          <div class="col-md-4">
+
+            <label class="form-label">วันเกิด <span style="color: red;">*</span></label>
+            <select class="form-select" name="s_day">
+              <option>กรุณาเลือกวันเกิด</option>
+              <option>1</option>
+              <option>2</option>
+              <option>3</option>
+              <option>4</option>
+              <option>5</option>
+              <option>6</option>
+              <option>7</option>
+              <option>8</option>
+              <option>9</option>
+              <option>10</option>
+              <option>11</option>
+              <option>12</option>
+              <option>13</option>
+              <option>14</option>
+              <option>15</option>
+              <option>16</option>
+              <option>17</option>
+              <option>18</option>
+              <option>19</option>
+              <option>20</option>
+              <option>21</option>
+              <option>22</option>
+              <option>23</option>
+              <option>24</option>
+              <option>25</option>
+              <option>26</option>
+              <option>27</option>
+              <option>28</option>
+              <option>29</option>
+              <option>30</option>
+              <option>31</option>
+            </select>
+
+          </div>
+          <div class="col-md-4">
+
+            <label class="form-label">เดือนเกิด <span style="color: red;">*</span></label>
+            <select class="form-select" name="s_month">
+              <option>กรุณาเลือกเดือนเกิด</option>
+              <option>มกราคม</option>
+              <option>กุมภาพันธ์</option>
+              <option>มีนาคม</option>
+              <option>เมษายน</option>
+              <option>พฤษภาคม</option>
+              <option>มิถุนายน</option>
+              <option>กรกฎาคม</option>
+              <option>สิงหาคม</option>
+              <option>กันยายน</option>
+              <option>ตุลาคม</option>
+              <option>พฤศจิกายน</option>
+              <option>ธันวาคม</option>
+            </select>
+          
+          </div>
+          <div class="col-md-4">
+
+            <label class="form-label">ปีเกิด <span style="color: red;">*</span></label>
+            <select class="form-select" name="s_year">
+              <option>กรุณาเลือกปีเกิด</option>
+              <?php for($i = 2500; $i < 2566; $i++) { ?>
+                <option><?= $i ?></option>
+              <?php } ?>
+            </select>
+          
+          </div>
+        </div>
+
+        <div class="row mt-3">
+          <div class="col-md-12">
+
+              <button type="submit" class="btn btn-primary">สมัครสมาชิก</button>
+
+          </div>
+        </div>
+
+      </form>
+
     </div>
 
     <?php include("./components/script.php"); ?>
