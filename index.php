1c5b7085ae7e674316ef8650d442f45d7b1146a6
Morakot Thongprom <tonmorakot@gmail.com>
2023-02-09 10:57:24 +0700

    WebFree 1.0.1

--- index.php
+++ index.php
@@ -8,7 +8,8 @@
     <?php include("./components/nav.php"); ?>
 
     <div class="container-fluid mt-3">
-      <h1>หน้าหลัก</h1>  
+      <h1>หน้าหลัก</h1>
+      <p>ระบบฐานข้อมูลนักศึกษา</p>
     </div>
 
     <?php include("./components/script.php"); ?>
